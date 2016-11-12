926e4a46869586fa382678cc3e38db02b2cfaa9a
Keeton Feavel <keetonfeavel@outlook.com>
2016-11-12 15:43:37 -0500

    Fixed Google Sign-in Crash

--- Remember/Login/WelcomeViewController.m
+++ Remember/Login/WelcomeViewController.m
@@ -56,12 +56,13 @@
     [corners createViewWithRoundedCornersWithRadius:10.0 andView:_blurView];
     
     _signIn = [GIDSignIn sharedInstance];
+    _signIn.delegate = self;
+    _signIn.uiDelegate = self;
     _signIn.shouldFetchBasicProfile = YES;
     //_signIn.allowsSignInWithWebView = YES;
     _signIn.clientID = @"262401164415-hqcftmico35rqpotenujfcbbrgl4uej6.apps.googleusercontent.com";
     _signIn.scopes = @[@"profile"];
-    _signIn.delegate = self;
-    _signIn.uiDelegate = self;
+    
     _image = [UIImage new];
     _photo = [[RMPhotoManager alloc] initWithView:self andFileName:@"MainPhoto"];
 }
